7fb221388eb4f9bf2a1b64d3f463121fc9eb74fa
giordanovita <giordanovitag@libero.it>
2021-03-10 21:33:05 +0100

    ex completo, alcune correzioni da fare

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var btnGenerator = document.getElementById('genera');
 var btnCancel = document.getElementById('annulla');
 var price = 0.21;
-var totalPrice = 0;
+var totalPrice = 0  ;
 var tariff = 'Prezzo pieno';
 
 
@@ -17,32 +17,31 @@ btnGenerator.addEventListener('click', function(){
         
         totalPrice = price * distance;
 
-        console.log(totalPrice)
+        //console.log(totalPrice)
         if ( age == 1){
             console.log('minorenne', age);
             var sconto = totalPrice * 0.2;
             totalPrice = totalPrice - sconto;
-            tariff += ' con lo sconto del 20%';
+            tariff = 'Sconto Under18';
         } else if (age == 3){
             console.log('over65', age)
             var sconto = totalPrice * 0.4;
             totalPrice = totalPrice - sconto;
-            tariff += ' con lo sconto del 40%';
+            tariff = 'Sconto Over65';
 
         }
 
 /* blocco else if per applicazione sconto
  */
-    parseFloat(document.getElementById('totalPrice').innerHTML = totalPrice);
+    parseFloat(document.getElementById('totalPrice').innerHTML = totalPrice.toFixed(2)) ;
     var cp = Math.floor(Math.random() * 10000) +90000 ;
-        document.getElementById('codCp').innerHTML = cp
+        document.getElementById('codCp').innerHTML = '<span class=result>' + cp + '</span>'
 
     var carriage = Math.floor(Math.random() * 40) + 1 ;
         document.getElementById('carriage').innerHTML = carriage
 
     document.getElementById('tariff').innerHTML = tariff;
-    document.getElementById('client').innerHTML = nameC;
-    
+    document.getElementById('client').innerHTML += '<br>' + nameC;
     
 
    }else{
